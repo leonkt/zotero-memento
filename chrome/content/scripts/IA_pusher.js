b529da95f0f31d4abf4a84625beeb23febf32684
leontk <leontk@pn1243256.lanl.gov>
2019-06-25 16:20:45 -0600

    Added archived link retrieval

--- chrome/content/scripts/IA_pusher.js
+++ chrome/content/scripts/IA_pusher.js
@@ -1,21 +1,50 @@
-
+/*
+ * Creates URL to send request to, in order to archive the URI passed in.
+ *
+ * @param uri: URI of page to archive
+ * @return: modified URI to archive the passed in URI.
+ */
 
 function constructURI(uri) {
 	return "https://web.archive.org/save/" + uri;
 }
-
-
-function push(uri) {
-	const request = require('request');
-	const init = {
-		method: 'GET',
-		uri: "https://int.lanl.gov/"
-	};
-	request(init, function(error, response, body) {
-		console.error(error);
-		console.log(response && response.statusCode);
-		console.log(body);
-	});
+/*
+ * Parses server response to find URL of archived resource.
+ *
+ * @param response: Server response as text
+ * @return: URI of the archived version of the webpage.
+ */
+function parseResponse(response) {
+	locationField = response.caseless.get("Location");
+	contentLocationField = response.caseless.get("Content-Location");
+	if (response && locationField) {
+		return locationField;
+	}
+	else if (response && contentLocationField) {
+		archivedUri = "https://web.archive.org" + contentLocationField;
+		return archivedUri;
+	}
+	else {
+		return "Cannot be archived";
+	}
 }
 
-push("https://www.w3schools.com/");
+/*
+ * Pushes a given webpage to the Internet Archive.
+ * 
+ * @param uri: URI of the page to archive
+ * @return: URI of the archived version of the webpage.
+ */
+async function push(uri) {
+	var req = require('request');
+	const init = {
+		method: 'GET',
+		uri: constructURI(uri),
+		resolveWithFullResponse: true
+	};
+	req(init);
+}
+
+
+
+push("https://jefferson.sgusd.k12.ca.us/");
