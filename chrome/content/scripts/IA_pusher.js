21fe5a1e286dea46a1a62ab98c943182ff487c71
leontk <leontk@pn1243256.lanl.gov>
2019-06-27 10:00:22 -0600

    changes to import scripts successfully

--- chrome/content/scripts/IA_pusher.js
+++ chrome/content/scripts/IA_pusher.js
@@ -1,11 +1,3 @@
-/*
- * TODO: Add wrapper callback function that uses the result of push() and parseResponse() 
- * to create item in Zotero with the given archived link...  
- */
-
-function pushLinkToZotero() {
-
-}
 
 /*
  * Creates URL to send request to, in order to archive the URI passed in.
@@ -28,35 +20,21 @@ function parseResponse(error, response, body) {
 	locationField = response.caseless.get("Location");
 	contentLocationField = response.caseless.get("Content-Location");
 	if (error) {
-		console.log(error);
+		return error;
 	}
 	else if (response && locationField) {
-		console.log(locationField);
+		return locationField;
 	}
 	else if (response && contentLocationField) {
 		archivedUri = "https://web.archive.org" + contentLocationField;
-		console.log(archivedUri);
+		return archivedUri;
 	}
 	else {
-		console.log("Cannot be archived");
+		return -1;
 	}
 }
 
-/*
- * Pushes a given webpage to the Internet Archive.
- * 
- * @param uri: URI of the page to archive
- * @return: URI of the archived version of the webpage.
- */
-function push(uri) {
-	var req = require('request');
-	const init = {
-		method: 'GET',
-		uri: constructURI(uri),
-	};
-	req(init, parseResponse);
-}
 
 
 
-push("https://stackoverflow.com/questions/4114095/how-do-i-revert-a-git-repository-to-a-previous-commit");
+
--- chrome/content/scripts/z_memento.js
+++ chrome/content/scripts/z_memento.js
@@ -1,8 +1,43 @@
-var pane = Zotero.getActiveZoteroPane();
-var items = pane.getSelectedItems();
+/*
+ * Pushes a given webpage to the Internet Archive.
+ * 
+ * @param uri: URI of the page to archive
+ * @return: URI of the archived version of the webpage.
+ */
+/*function push(item) {
+	var req = require('request');
+	
+	var init = {
+		method: 'GET',
+		uri: pusher.constructURI(item.getField('url'))
+	};
+	alert(item.getField('url'));
+	req(init, function(error, response, body) {
+		item.setField('extra', pusher.parseResponse(error, response, body));
+	});
+}*/
 
-function addBeetleJuice() {
-	var item = new Zotero.Item('Book');
-	item.setField('title', 'Beetlejuice');
+function modifyExtra() {
+	var req = require('request');
+	var pusher = require('IA_pusher');
+	var pane = Zotero.getActiveZoteroPane();
+	var selectedItems = pane.getSelectedItems();
+	var item = selectedItems[0];
+	var partialURI = item.getField('url');
+	var init = {
+		method: 'GET',
+		uri: pusher.constructURI(partialURI)
+	};
+	alert(tem.getField('url'));
+	req(init, function(error, response, body) {
+		item.setField('extra', pusher.parseResponse(error, response, body));
+		item.save();
+	});
+}
+
+function test() {
+	var pane = Zotero.getActiveZoteroPane();
+	var item = new Zotero.Item('book');
+	item.setField('title', "beetlejuice");
 	item.save();
 }
