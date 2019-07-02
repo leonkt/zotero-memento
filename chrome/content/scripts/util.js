f0c326497c56cd2e0970c2b11469486fe502f8b5
leontk <leontk@pn1243256.lanl.gov>
2019-07-02 13:29:46 -0600

    function comments in util

--- chrome/content/scripts/util.js
+++ chrome/content/scripts/util.js
@@ -46,6 +46,14 @@ function createCORSRequest(method, url) {
   return xhr;
 }
 
+/*
+ * Extracts the URL from the server response if a well-formed one cannot be found from the 
+ * location or content-location headers.
+ *
+ * @param {string} responseText: server response represented as text.
+ *
+ * @return {string}: well-formed part of URL to access the archived resource.
+ */
 
 function extractUrl(responseText) {
   var start = responseText.indexOf("redirUrl") + 12;
@@ -53,14 +61,32 @@ function extractUrl(responseText) {
   return responseText.slice(start, end);
 }
 
-function checkWellFormedUrl(url) {
+/*
+ * Makes sure that portion of the URL following the first "/" is representative of an 
+ * archived resource.
+ *
+ * @param {string} url: part of the URL to check.
+ *
+ * @return {boolean}: whether url matches the given pattern.
+ */
+function isWellFormedUrl(url) {
   var pattern = /\/web\/[0-9]+\/.+/
   return pattern.test(url);
 }
 
+/*
+ * Modifies the "extra" field of the given item to contain a link to the archived resource.
+ *
+ * @param {Zotero.Item} item: the library entry to be modified.
+ * @param {string} cLoc: the value of the content-location header in the server response.
+ * @param {string} loc: the value of the location-header in the server response.
+ * @param {string} responseText: server response represented as text.
+ *
+ * @returns: nothing.
+ */
 function modifyExtra(item, cLoc, loc, responseText) {
   if (loc) {
-    if (checkWellFormedUrl(loc)) {
+    if (isWellFormedUrl(loc)) {
       item.setField("extra", "Archived Link: " + loc);
     }
     else {
@@ -69,7 +95,7 @@ function modifyExtra(item, cLoc, loc, responseText) {
     }
   }
   else if (cLoc) {
-    if (checkWellFormedUrl(cLoc)) {
+    if (isWellFormedUrl(cLoc)) {
       item.setField("extra",  "Archived Link: http://web.archive.org" + cLoc);
     }
     else {
@@ -82,6 +108,10 @@ function modifyExtra(item, cLoc, loc, responseText) {
     item.setField("extra", "Archive URL not found.");
   }
   item.saveTx();
+}
+
+function handleError() {
+
 }
 /*
  * Sends the request to archive a given resource. Sets the content of the extra field
@@ -105,6 +135,10 @@ function sendReq() {
       loc = req.getResponseHeader("location");
       var responseText = req.responseText;
       modifyExtra(item, cLoc, loc, responseText);
+    }
+    else if (req.status != 200) {
+      // Popup that says failed to archive in the IA
+      //handleError();
     }
   }
   req.send();
